cec560ea2f47e4249b3fc5c0e8e2d951a15fd8d2
4lper <alper@veri.zone>
2018-11-28 13:36:28 +0300

    yasak kelimeleri dosyaya alma

--- config/geoip.php
+++ config/geoip.php
@@ -128,17 +128,17 @@ return [
     'default_location' => [
         'ip' => '127.0.0.0',
         'iso_code' => 'US',
-        'country' => 'United States',
-        'city' => 'New Haven',
-        'state' => 'CT',
+        'country' => 'Türkiye',
+        'city' => 'Ankara',
+        'state' => 'Çankaya',
         'state_name' => 'Connecticut',
         'postal_code' => '06510',
         'lat' => 41.31,
         'lon' => -72.92,
-        'timezone' => 'America/New_York',
+        'timezone' => 'Europe/Istanbul',
         'continent' => 'NA',
         'default' => true,
-        'currency' => 'USD',
+        'currency' => 'TRY',
     ],
 
 ];
