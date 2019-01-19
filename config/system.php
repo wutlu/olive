20cc52d9114a3a9498af13865643e52beaa92b9e
4lper <alper@veri.zone>
2019-01-19 19:32:39 +0300

    markdown break

--- config/system.php
+++ config/system.php
@@ -4,7 +4,7 @@ return [
     /**
      * Sistem Versiyonu
      */
-    'version' => '1.3.1031-alpha',
+    'version' => '1.3.1032-alpha',
 
     /**
      * Gizlilik Politikası ve Kullanım Koşulları Versiyonu
