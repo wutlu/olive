bdd3e7d72ab688341266bde880e2f330615050f9
4lper <alper@veri.zone>
2019-01-29 17:50:25 +0300

    .

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -1,11 +1,11 @@
 @extends('errors::illustrated-layout')
 
 @section('code', '503')
-@section('title', __('Bakım Modu'))
+@section('title', __('Servis Modu'))
 
 @section('image')
 <div style="background-image: url({{ asset('/svg/503.svg') }});" class="absolute pin bg-cover bg-no-repeat md:bg-left lg:bg-center">
 </div>
 @endsection
 
-@section('message', __($exception->getMessage() ?: 'Yedekleme ve bakım için ara verdik. En geç 10 dakika içerisinde tekrar aktif olacağız.'))
+@section('message', __($exception->getMessage() ?: 'Üzgünüz, biraz bakım yapıyoruz. Lütfen kısa süre sonra tekrar deneyin.'))
