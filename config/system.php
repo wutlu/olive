d9e177bf7c4d380e7377632d63c6cabc4107759f
4lper <alper@veri.zone>
2019-01-30 16:47:29 +0300

    card with bg

--- config/system.php
+++ config/system.php
@@ -4,7 +4,7 @@ return [
     /**
      * Sistem Versiyonu
      */
-    'version' => '1.4.02-alpha',
+    'version' => '1.4.03-alpha',
 
     /**
      * Gizlilik Politikası ve Kullanım Koşulları Versiyonu
