b6d71cf09fff5673641238364111561e77b7e1d7
4lper <alper@veri.zone>
2019-02-08 21:34:38 +0300

    yatırımcı paketi kapandı.

--- config/plans.php
+++ config/plans.php
@@ -1,26 +1,25 @@
 <?php
 
 return [
-	2 => [
-		'name' => 'Yatırımcı',
-		'description' => 'Olive, henüz geliştirme aşamasında olduğundan tüm planlar satışa sunulmamıştır. Sadece geliştirme süresince alabileceğiniz YATIRIM planını satın alarak, hazır ve hazırlanmakta olan tüm Olive özelliklerini tam erişim sağlayabilirsiniz. Bu plan ilerleyen zamanlarda kaldırılacaktır. Fakat siz her zaman bu planı bu fiyattan kullanmaya devam edebilecek ve yılda '.config('formal.currency').' 8.520 kâr edeceksiniz.',
-		'price' => 270,
+	1 => [
+		'name' => 'Bireysel',
+		'price' => 490,
 		'buy' => true,
+		'class' => 'active',
 		'properties' => [
 			'capacity' => [
 				'text' => 'Plan Kapasitesi',
-				'details' => 'Plan kapasitesi kadar kullanıcı aynı anda bu planı kullanabilir.',
-				'value' => 2
+				'value' => 1
 			],
 			'keyword_groups' => [
 				'text' => 'Kelime Grubu',
 				'details' => '',
-				'value' => 2
+				'value' => 1
 			],
 			'keyword_lines' => [
 				'text' => 'Kelime Satırı',
 				'details' => '',
-				'value' => 4
+				'value' => 2
 			],
 			'support' => [
 				'text' => 'Destek',
@@ -30,40 +29,13 @@ return [
 		],
 		'class' => 'teal-text active'
 	],
-	3 => [
-		'name' => 'Bireysel',
-		'price' => 490,
-		'properties' => [
-			'capacity' => [
-				'text' => 'Plan Kapasitesi',
-				'details' => 'Plan kapasitesi kadar kullanıcı aynı anda bu planı kullanabilir.',
-				'value' => 1
-			],
-			'keyword_groups' => [
-				'text' => 'Kelime Grubu',
-				'details' => '',
-				'value' => 1
-			],
-			'keyword_lines' => [
-				'text' => 'Kelime Satırı',
-				'details' => '',
-				'value' => 2
-			],
-			'support' => [
-				'text' => 'Destek',
-				'details' => '7/24 Online Destek',
-				'value' => true
-			],
-		],
-		'class' => 'teal-text'
-	],
-	4 => [
-		'name' => 'Organize',
+	2 => [
+		'name' => 'Standart',
 		'price' => 1960,
+		'buy' => true,
 		'properties' => [
 			'capacity' => [
 				'text' => 'Plan Kapasitesi',
-				'details' => 'Plan kapasitesi kadar kullanıcı aynı anda bu planı kullanabilir.',
 				'value' => 4
 			],
 			'keyword_groups' => [
@@ -84,13 +56,13 @@ return [
 		],
 		'class' => 'teal-text'
 	],
-	5 => [
+	3 => [
 		'name' => 'Kurumsal',
 		'price' => 4120,
+		'buy' => true,
 		'properties' => [
 			'capacity' => [
 				'text' => 'Plan Kapasitesi',
-				'details' => 'Plan kapasitesi kadar kullanıcı aynı anda bu planı kullanabilir.',
 				'value' => 8
 			],
 			'keyword_groups' => [
