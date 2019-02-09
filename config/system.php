3759d2fd751b3d2bb8b4e1dab40b3edbcbfbbe60
4lper <alper@veri.zone>
2019-02-09 22:00:19 +0300

    version

--- config/system.php
+++ config/system.php
@@ -4,7 +4,7 @@ return [
     /**
      * Sistem Versiyonu
      */
-    'version' => '1.4.11-alpha',
+    'version' => '1.4.12-alpha',
 
     /**
      * Gizlilik Politikası ve Kullanım Koşulları Versiyonu
