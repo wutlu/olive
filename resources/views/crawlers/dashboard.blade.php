e60f4d091ff5a25d4e74cdc236842c075a72322e
4lper <alper@veri.zone>
2019-02-17 04:17:32 +0300

    .

--- resources/views/crawlers/dashboard.blade.php
+++ resources/views/crawlers/dashboard.blade.php
@@ -11,28 +11,24 @@
 ])
 
 @section('content')
-    <a href="{{ route('crawlers.media.list') }}" class="d-block card-panel hoverable waves-effect" data-tooltip="Medya Botları" data-position="right">
+    <a href="{{ route('crawlers.media.list') }}" class="d-block card-panel hoverable" data-tooltip="Medya Botları" data-position="right">
         <i class="material-icons">widgets</i>
         <span class="d-block">Medya Botları</span>
     </a>
-    <a href="{{ route('crawlers.sozluk.list') }}" class="d-block card-panel hoverable waves-effect" data-tooltip="Sözlük Botları" data-position="right">
+    <a href="{{ route('crawlers.sozluk.list') }}" class="d-block card-panel hoverable" data-tooltip="Sözlük Botları" data-position="right">
         <i class="material-icons">widgets</i>
         <span class="d-block">Sözlük Botları</span>
     </a>
-    <a href="{{ route('crawlers.shopping.list') }}" class="d-block card-panel hoverable waves-effect" data-tooltip="E-ticaret Botları" data-position="right">
+    <a href="{{ route('crawlers.shopping.list') }}" class="d-block card-panel hoverable" data-tooltip="E-ticaret Botları" data-position="right">
         <i class="material-icons">widgets</i>
         <span class="d-block">E-ticaret Botları</span>
     </a>
-    <a href="{{ route('admin.twitter.settings') }}" class="d-block card-panel hoverable waves-effect" data-tooltip="Twitter Ayarları" data-position="right">
+    <a href="{{ route('admin.twitter.settings') }}" class="d-block card-panel hoverable" data-tooltip="Twitter Ayarları" data-position="right">
         <i class="material-icons">widgets</i>
         <span class="d-block">Twitter Ayarları</span>
     </a>
-    <a href="{{ route('admin.youtube.settings') }}" class="d-block card-panel hoverable waves-effect" data-tooltip="YouTube Ayarları" data-position="right">
+    <a href="{{ route('admin.youtube.settings') }}" class="d-block card-panel hoverable" data-tooltip="YouTube Ayarları" data-position="right">
         <i class="material-icons">widgets</i>
         <span class="d-block">YouTube Ayarları</span>
-    </a>
-    <a href="{{ route('admin.google.settings') }}" class="d-block card-panel hoverable waves-effect" data-tooltip="Google Ayarları" data-position="right">
-        <i class="material-icons">widgets</i>
-        <span class="d-block">Google Ayarları</span>
     </a>
 @endsection
