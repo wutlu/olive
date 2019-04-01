8bb0bf9655135afdb2ffdb3bcfade81495026588
4lper <alper@veri.zone>
2019-04-01 17:24:33 +0300

    .

--- app/Console/Commands/Crawlers/Media/Alexa.php
+++ app/Console/Commands/Crawlers/Media/Alexa.php
@@ -4,11 +4,19 @@ namespace App\Console\Commands\Crawlers\Media;
 
 use Illuminate\Console\Command;
 
-use App\Elasticsearch\Document;
-
-use Carbon\Carbon;
-
 use App\Models\Crawlers\MediaCrawler;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Cookie\CookieJar;
+
+use App\Wrawler;
+
+use App\Models\Proxy;
+
+use System;
+
+use DB;
 
 class Alexa extends Command
 {
@@ -43,7 +51,9 @@ class Alexa extends Command
      */
     public function handle()
     {
-        $crawlers = MediaCrawler::get();
+        $crawlers = MediaCrawler::where('test', true)
+                                ->where('error_count', '<', DB::raw("off_limit"))
+                                ->get();
 
         if (count($crawlers))
         {
@@ -51,24 +61,66 @@ class Alexa extends Command
             {
                 $this->line($crawler->site);
 
-                $xml = simplexml_load_file('http://data.alexa.com/data?cli=10&dat=snbamz&url='.$crawler->site);
+                $client = new Client([
+                    'base_uri' => 'http://data.alexa.com',
+                ]);
 
-                $stats = isset($xml->SD[1]->COUNTRY) ? $xml->SD[1]->COUNTRY->attributes() : null;
+                $arr = [
+                    'timeout' => 10,
+                    'connect_timeout' => 5,
+                    'headers' => [
+                        'User-Agent' => config('crawler.user_agents')[array_rand(config('crawler.user_agents'))],
+                        'Accept' => 'application/xml'
+                    ],
+                    'verify' => false,
+                    'query' => [
+                        'cli' => 10,
+                        'dat' => 's',
+                        'url' => $crawler->site
+                    ]
+                ];
 
-                if ($stats)
+                try
                 {
-                    $rank = ((array) $stats->RANK)[0];
+                    $p = Proxy::where('health', '>', 7)->inRandomOrder()->first();
 
-                    $this->info(number_format($rank));
+                    if (@$p)
+                    {
+                        $arr['proxy'] = $p->proxy;
+                    }
 
-                    $crawler->status = ($rank < 20000) ? true : false;
-                    $crawler->alexa_rank = $rank;
-                    $crawler->off_reason = $crawler->off_reason ? $crawler->off_reason : 'Alexa sıralaması çok düşük olduğundan görev sonlandırıldı.';
-                    $crawler->update();
+                    $xml = $client->get('data', $arr)->getBody();
+
+                    $saw = new Wrawler($xml);
+                    $meta_property = $saw->get('popularity')->toArray();
+
+                    $rank = @$meta_property[0]['text'] ? $meta_property[0]['text'] : null;
+
+                    if ($rank)
+                    {
+                        $this->info(number_format($rank));
+
+                        $crawler->status = ($rank < config('app.alexa.rank_min')) ? true : false;
+                        $crawler->alexa_rank = $rank;
+                        $crawler->off_reason = $crawler->off_reason ? $crawler->off_reason : 'Alexa sıralaması çok düşük olduğundan görev sonlandırıldı.';
+                        $crawler->update();
+                    }
+                    else
+                    {
+                        $this->error('Alexa verisi alınamadı.');
+                    }
                 }
-                else
+                catch (\Exception $e)
                 {
-                    $this->error('Alexa verisi alınamadı.');
+                    $this->error($e->getMessage());
+
+                    System::log(
+                        json_encode(
+                            $e->getMessage()
+                        ),
+                        'App\Console\Commands\Crawlers\Media\Alexa::create('.$crawler->id.')',
+                        10
+                    );
                 }
             }
         }
