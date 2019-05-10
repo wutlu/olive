c8d9f0670a9d31787306a07c9ea2097927a406e9
4lper <alper@veri.zone>
2019-05-10 16:09:28 +0300

    .

--- resources/views/pin/pins.blade.php
+++ resources/views/pin/pins.blade.php
@@ -60,11 +60,9 @@
 
         @if ($document->status == 'ok')
             @php
-            $id = $document->data['_index'].'_'.$document->data['_type'].'_'.$document->data['_id'];
-            $type = $document->data['_type'];
-            $source = $document->data['_source'];
-
-            $sentiment = @$source['sentiment'];
+                $id = $document->data['_index'].'_'.$document->data['_type'].'_'.$document->data['_id'];
+                $type = $document->data['_type'];
+                $source = $document->data['_source'];
             @endphp
 
             <ul id="dropdown-{{ $id }}" class="dropdown-content">
@@ -209,29 +207,6 @@
                                 data-type="{{ $document->data['_type'] }}"
                                 data-id="{{ $document->data['_id'] }}">{{ $pin->comment }}</textarea>
                             <label for="textarea-{{ $id }}">Yorum Girin</label>
-                        </div>
-                    </div>
-                @endif
-
-                @if ($sentiment)
-                    <div class="card-sentiment d-flex justify-content-between">
-                        <div style="width: {{ $sentiment['pos']*100 }}%;" class="sentiment-item light-green-text accent-4 d-flex">
-                            @if ($sentiment['pos'] > 0.2)
-                            <i class="material-icons light-green-text align-self-center">sentiment_very_satisfied</i>
-                            <span class="badge light-green-text align-self-center">{{ $sentiment['pos']*100 }}%</span>
-                            @endif
-                        </div>
-                        <div style="width: {{ $sentiment['neu']*100 }}%;" class="sentiment-item grey-text d-flex">
-                            @if ($sentiment['neu'] > 0.2)
-                            <i class="material-icons grey-text text-darken-2 align-self-center">sentiment_neutral</i>
-                            <span class="badge grey-text text-darken-2 align-self-center">{{ $sentiment['neu']*100 }}%</span>
-                            @endif
-                        </div>
-                        <div style="width: {{ $sentiment['neg']*100 }}%;" class="sentiment-item red-text accent-4 d-flex">
-                            @if ($sentiment['neg'] > 0.2)
-                            <i class="material-icons red-text align-self-center">sentiment_very_dissatisfied</i>
-                            <span class="badge red-text align-self-center">{{ $sentiment['neg']*100 }}%</span>
-                            @endif
                         </div>
                     </div>
                 @endif
