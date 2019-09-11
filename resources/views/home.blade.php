bebee6dc528732a78228d7e75b054b518bd95396
qhudabaksh <alper@veri.zone>
2019-09-11 12:33:15 +0300

    .

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,8 @@
 @extends('layouts.app', [
     'footer_extend' => true,
-    'title' => 'Veri Zone Teknoloji',
+    'title' => [
+        'text' => 'Veri Zone Teknoloji'
+    ],
     'description' => 'Veri Zone Teknoloji, sosyal medya ve haber verilerine; üstün filtreleme ve çeşitli analiz özellikleriyle, daha hızlı ve daha anlamlı bir şekilde ulaşmanızı sağlayan yazılımlar geliştirir.'
 ])
 
