4a6c30827dc8a47e96f6c79797a1c47a425e0476
qhudabaksh <alper@veri.zone>
2019-10-11 15:01:57 +0300

    .

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -35,7 +35,7 @@ class PageController extends Controller
      */
     public static function adminListView(int $pager = 10)
     {
-        $pages = Page::whereNotIn('slug', [ 'satis-sozlesmesi' ])->paginate($pager);
+        $pages = Page::whereNotIn('slug')->paginate($pager);
 
         return view('page.admin.list', compact('pages'));
     }
