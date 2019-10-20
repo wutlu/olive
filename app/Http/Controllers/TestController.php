7dc15ece815461e2ea4e748fac5280851409d06b
qhudabaksh <alper@veri.zone>
2019-10-20 20:30:03 +0300

    log

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -3,24 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Str;
-
-use App\Models\Crawlers\MediaCrawler;
-use System;
-use Term;
 
 class TestController extends Controller
 {
     public static function test(Request $request)
     {
-    	$cr = MediaCrawler::get();
 
-    	foreach ($cr as $craw)
-    	{
-	    	$name = $craw->site;
-
-
-	    	echo PHP_EOL;
-    	}
     }
 }
