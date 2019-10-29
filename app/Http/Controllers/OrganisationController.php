426ee2e66759835580b0d6afebf82046939c7a76
qhudabaksh <alper@veri.zone>
2019-10-29 20:25:35 +0300

    .

--- app/Http/Controllers/OrganisationController.php
+++ app/Http/Controllers/OrganisationController.php
@@ -154,24 +154,26 @@ class OrganisationController extends Controller
                 ];
             }
 
-            $source_count = 0;
-
-            /**
-             * modules
-             */
-            foreach (config('system.modules') as $key => $module)
+            if ($request->module_real_time || $request->module_search || $request->module_alarm)
             {
-                $source_count = $request->{'data_'.$key} ? ($source_count+1) : $source_count;
-            }
+                $source_count = 0;
 
-            if (!$source_count)
-            {
-                return [
-                    'status' => 'err',
-                    'reason' => 'Canlı Akış, Arama veya Alarm seçiliyken en az 1 veri kaynağı seçmeniz gerekiyor.'
-                ];
-            }
+                /**
+                 * modules
+                 */
+                foreach (config('system.modules') as $key => $module)
+                {
+                    $source_count = $request->{'data_'.$key} ? ($source_count+1) : $source_count;
+                }
 
+                if (!$source_count)
+                {
+                    return [
+                        'status' => 'err',
+                        'reason' => 'Canlı Akış, Arama veya Alarm seçiliyken en az 1 veri kaynağı seçmeniz gerekiyor.'
+                    ];
+                }
+            }
 
             $organisation = new Organisation;
             $organisation->user_id = $user->id;
